75643f3c9cd4b7366737f73187497dc3635f1b6c
Кирилл Терещенко <elpuffru@gmail.com>
2017-11-22 12:29:26 +0300

    Add sections, edit menu links

---
+++ partneram/kak-stat-partnerom/index.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords", "мягкая мебель, офисная мебель, мебель для кухни, детская мебель");
+$APPLICATION->SetPageProperty("description", "Мебельная компания осуществляет производство мебели на высококлассном оборудовании с применением минимальной доли ручного труда, что позволяет обеспечить высокое качество нашей продукции.");
+$APPLICATION->SetPageProperty("ROBOTS", "/index, follow");
+$APPLICATION->SetTitle("Как стать партнером");
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
